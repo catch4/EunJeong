ae7905eaad99347876c142f567d7626ec03b6162
은정 Rachel <50548458+iamkkwak@users.noreply.github.com>
2020-06-15 19:59:37 +0900

    프로그래머스 17687. [3차] n진수 게임

---
+++ June 3rd/P17687.cpp
@@ -0,0 +1,33 @@
+// https://programmers.co.kr/learn/courses/30/lessons/17687
+
+#include <string>
+#include <vector>
+
+using namespace std;
+const char HEX[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+// 숫자 x을 n진수로
+string convert(int x, int n) {
+    if (x == 0) return "0";
+    string ret = "";
+    while (x) {
+        ret = HEX[x % n] + ret;
+        x /= n;
+    }
+    return ret;
+}
+
+string solution(int n, int t, int m, int p) {
+    string answer = "";
+    int turn = 0, num = 0;
+    while(true) {
+        string tmp = convert(num, n);
+        for (int i = 0; i < tmp.length(); i++) {
+            turn++;
+            if (turn % m == (p % m)) answer += tmp[i];
+            if (answer.size() == t) return answer;
+        }
+        num++;
+    }
+    return answer;
+}
