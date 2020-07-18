84256d7f9d218db1069a5533a29510c98a85f49e
은정 Rachel <50548458+iamkkwak@users.noreply.github.com>
2020-07-18 13:00:18 +0900

    백준 2352. 반도체 설계

--- 2020-06-08/B2352.cpp
+++ 2020-06-08/B2352.cpp
@@ -3,7 +3,7 @@
 // 내가 아는 O(N^2) 단순 DP 방법을 쓰면 당연히 시간초과가 날 것 같았다^^..
 // 그래서 혹시나 시간복잡도가 더 작은 LIS 알고리즘이 있는지 찾아봤는데,
 // 이분탐색을 이용한 O(NlgN) 방식이 있길래 해당 방식을 공부하고 풀었다.
-// (탐색은 구현하기 귀찮아서 그냥 algorithm 헤더 썼다.. ㅎㅎㅎ)
+// (탐색은 구현하기 귀찮아서 그냥 algorithm 헤더 썼다.. ㅎ_ㅎ)
 
 #include <iostream>
 #include <algorithm>
@@ -31,8 +31,8 @@ int main() {
 			int ii = lower_bound(DP, DP + idx, Port[i]) - DP;
 			DP[ii] = Port[i];
 		}
-	}
-
+	} 
+	
 	cout << idx << endl;
 	return 0;
 }
